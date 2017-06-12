809baa05dc058eaddeb4122275abe3de78696c61
soberlaender <soberlaender@student.tugraz.at>
2017-06-12 21:21:56 +0200

    changes

--- startProject.py
+++ startProject.py
@@ -9,6 +9,11 @@ import io
 from nltk.sentiment import vader
 sia = vader.SentimentIntensityAnalyzer()
 
+hostname = 'localhost'
+username = 'root'
+password = ''
+database = 'twitterdata_4'
+
 def vaderSentiment(tweet):
     return sia.polarity_scores(tweet)['compound']
 
@@ -17,34 +22,32 @@ def getReviewSentiments(sentimentCalculator, listOfElements):
     return [sentimentCalculator(tweet) for tweet in tweets]
 
 def getUniqueHashTag(conn):
-    return [["Something"], ["Health"], ["Sports"]]
-    # cur = conn.cursor()
-    # cur.execute("SELECT DISTINCT HashTag FROM  tweets WHERE userLanguage = 'en'")    
-    # return cur.fetchall()
+    #return [["Something"], ["Health"], ["Sports"]]
+     cur = conn.cursor()
+     cur.execute("SELECT DISTINCT HashTag FROM  tweets WHERE userLanguage = 'en'")    
+     return cur.fetchall()
 
 def getTweetData(tweet):
     return tweet[0]
 
 def getAllEnglishTweets(conn, hashtag):
-    return [["This is nice"], ["This is bad"], ["This is super nice"]]
-    # cur = conn.cursor()
+    #return [["This is nice"], ["This is bad"], ["This is super nice"]]
+     cur = conn.cursor()
     # Query also by hashtag use the hashtag param to query the db
-    # cur.execute("SELECT Text FROM tweets WHERE userLanguage = 'en'")    
-    # return cur.fetchall()
+     cur.execute("SELECT Text FROM tweets WHERE userLanguage = 'en'")    
+     return cur.fetchall()
 
 negativeReviewFileName = "/home/igorjakovljevic/NLPProjects/NLPToolkit/School/tweets.csv"
-# myConnection = MySQLdb.connect( host=hostname, user=username, passwd=password, db=database )
+myConnection = MySQLdb.connect( host=hostname, user=username, passwd=password, db=database )
 
-# hashtags = getUniqueHashTag(myConnection)
+hashtags = getUniqueHashTag(myConnection)
 
-hashtags = getUniqueHashTag("")
+#hashtags = getUniqueHashTag("")
 result = []
 for hashtag in hashtags:
-    # tweets = getAllEnglishTweets(myConnection, hashtag)
-    tweets = getAllEnglishTweets("", hashtag)
+    tweets = getAllEnglishTweets(myConnection, hashtag)
+    #tweets = getAllEnglishTweets("", hashtag)
     resultSentiment = getReviewSentiments(vaderSentiment,tweets)
     result.append({"hashtag": hashtag[0], "PositiveTweets" : sum([1 for x in resultSentiment if x > 0]), "negativeTweets": sum([1 for x in resultSentiment if x < 0])})
 
 print result
-
-
